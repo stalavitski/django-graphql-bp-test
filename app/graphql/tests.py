ad68354f282645f5827c1e0cf6653ee861bb0045
Artsem Stalavitski <a.stalavitski@gmail.com>
2018-02-06 05:56:19 +0300

    updated bp version

--- app/graphql/tests.py
+++ app/graphql/tests.py
@@ -1,8 +1,18 @@
 from app.graphql.api import schema
-from django_graphql_bp.user.tests import SchemaTestCase as UserSchemaTestCase
+from django_graphql_bp.user.tests import *
 from graphene import Schema
 
 
-class SchemaTestCase(UserSchemaTestCase):
+class GraphqlTestCase(cases.OperationTestCase):
     def get_schema(self) -> Schema:
         return schema
+
+    def setUp(self):
+        super(GraphqlTestCase, self).setUp()
+
+    def set_up_user(self):
+        self.user = self.create_test_user('user', {'username': 'user'})
+        self.user2 = self.create_test_user('user2', {'username': 'user2'})
+        self.staff = self.create_test_user('staff', {'username': 'staff'})
+        self.staff.is_staff = True
+        self.staff.save()
